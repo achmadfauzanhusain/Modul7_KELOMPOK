35cca2a8f4f199d44a266b869aa55d5f91861cc1
Your Name.com <you@example.com>
2026-04-14 08:07:29 +0700

    menambahkan class FilmFavorit_103022400139

---
+++ Modul7_KELOMPOK/FilmFavorit_103022400139.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+public class FilmFavorit_103022400139
+{
+    public string title { get; set; }
+    public string director { get; set; }
+    public string year { get; set; }
+    public string genre { get; set; }
+    public string rating { get; set; }
+    public string durationMinutes { get; set; }
+    public string isWatched { get; set; }
+
+    public static void ReadJSON()
+    {
+        string jsonString = File.ReadAllText("jurnal7_1_103022400139.json");
+
+        var data = JsonSerializer.Deserialize<FilmFavorit_103022400139 > (jsonString);
+
+        Console.WriteLine($"Judul film : {data.title} Nama director : {data.director} Tahun : {data.year} Genre : {data.genre} Rating : {data.rating}" +
+            $" Durasi : {data.durationMinutes} Sudah menonton? {data.isWatched}");
+    }
+}
